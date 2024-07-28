591a61ae635053a14cca3871051b762969650900
Masiat12 <masiatmohammad@gmail.com>
2024-07-29 00:23:28 +0600

    first commit

---
+++ foodi-client/src/components/Cards.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {FaHeart} from "react-icons/fa"
+
+const Cards = ({ item }) => {
+
+    const [isHeartfilled, setIsHeartfilled] = useState(false);
+
+    const handleHeartClick = () => {
+        setIsHeartfilled(!isHeartfilled)
+
+    }
+
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-xl relative">
+        <div className={`rating gap-1 absolute right-2 p-4 top-2 heartStar bg-green ${isHeartfilled ?
+        "text-rose-500" : "text-white" }`}
+        onClick={handleHeartClick}
+        >
+            <FaHeart className="h-5 w-5 cursor-pointer"/>
+
+        </div>
+        <Link to={`/menu/${item._id}`}>
+          <figure>
+            <img
+              src={item.image}
+              alt=""
+              className="hover:scale-105 transition-all duration-200 md:h-72"
+            />
+          </figure>
+        </Link>
+        <div className="card-body">
+          <h2 className="card-title">{item.name}</h2>
+          <p>Description</p>
+
+          <div className="card-actions justify-between items-center mt-2">
+            <h5 className="font-semibold ">
+              <span className="text-sm text-red">$</span>
+              {item.price}
+            </h5>
+            <button className="btn btn-primary bg-green text-white">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
+
+// import React, { useState } from "react";
+// import { FaHeart } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+
+// const Cards = ({ item }) => {
+//   // console.log(item)
+//   const [isHeartFilled, setIsHeartFilled] = useState(false);
+
+//   const handleHeartClick = () => {
+//     setIsHeartFilled(!isHeartFilled);
+//   };
+//   return (
+//     <div to={`/menu/${item._id}`} className="card shadow-xl relative mr-5 md:my-5">
+//       <div
+//         className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${
+//           isHeartFilled ? "text-rose-500" : "text-white"
+//         }`}
+//         onClick={handleHeartClick}
+//       >
+//         <FaHeart className="w-5 h-5 cursor-pointer" />
+//       </div>
+//       <Link to={`/menu/${item._id}`}>
+//         <figure>
+//           <img src={item.image} className="hover:scale-105 transition-all duration-300 md:h-72" />
+//         </figure>
+//       </Link>
+//       <div className="card-body">
+//        <Link to={`/menu/${item._id}`}><h2 className="card-title">{item.name}!</h2></Link>
+//         <p>Description of the item</p>
+//         <div className="card-actions justify-between items-center mt-2">
+//           <h5 className="font-semibold">
+//             <span className="text-sm text-red">$ </span> {item.price}
+//           </h5>
+//           <button className="btn bg-green text-white">Add to Cart </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Cards;
---
+++ foodi-client/src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Categories from './Categories'
+import Banner from '../../components/Banner'
+import SpecialDishes from './SpecialDishes'
+import Testimonials from './Testimonials'
+import OurServices from './OurServices'
+import Footer from '../../components/Footer'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <Categories/>
+      <SpecialDishes/>
+      <Testimonials/>
+      <OurServices/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
