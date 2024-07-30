61b01a992e92b22e1ae6ab96b6aa04382147b007
Masiat12 <masiatmohammad@gmail.com>
2024-07-30 22:57:15 +0600

    s

--- foodi-client/src/layout/Main.jsx
+++ foodi-client/src/layout/Main.jsx
@@ -2,13 +2,17 @@ import React from "react"
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import "../App.css"
+import Footer from "../components/Footer"
 
 const Main = () =>{
 
     return(
         <div>
             <Navbar/>
+            <div className="min-h-screen">
             <Outlet/>
+            </div>
+            <Footer/>
         
 
         </div>
